9ec42e74fb3b80e4d8f9eb66de6ae6bbd91139bc
Rodolfo Barrios <rodolfo.barrios@ceiba.com.co>
2019-09-10 06:41:04 -0500

    Agregando las clases databuilder

---
+++ domain/src/test/java/co/com/ceiba/testdatabuilder/HistorialTestDataBuilder.java
@@ -0,0 +1,37 @@
+package co.com.ceiba.testdatabuilder;
+
+import java.time.LocalDateTime;
+
+import co.com.ceiba.domain.modelo.Historial;
+import co.com.ceiba.domain.modelo.Vehiculo;
+
+public class HistorialTestDataBuilder {
+
+    private static final Vehiculo VEHICULO_CARRO_PRUEBA = new VehiculoTestDataBuilder().build();
+    private static final LocalDateTime FECHA_INGRESO_PRUEBA = LocalDateTime.of(2019, 8, 28, 7, 0);
+
+    private Vehiculo vehiculo;
+
+    private LocalDateTime fechaIngreso;
+
+    public HistorialTestDataBuilder() {
+        this.vehiculo = VEHICULO_CARRO_PRUEBA;
+        this.fechaIngreso = FECHA_INGRESO_PRUEBA;
+    }
+
+    public HistorialTestDataBuilder conVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+        return this;
+    }
+
+    public HistorialTestDataBuilder conFechaIngreso(LocalDateTime fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+        return this;
+    }
+
+    public Historial build() {
+        return new Historial(this.vehiculo, this.fechaIngreso);
+    }
+
+
+}
