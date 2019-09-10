e838380d10810c857dcb3f943e6c18269bdba1c0
Rodolfo Barrios <rodolfo.barrios@ceiba.com.co>
2019-09-10 16:03:30 -0500

    eliminando lo incesario

--- app/src/main/java/co/com/ceiba/clean/viewmodel/HistorialesViewModel.java
+++ app/src/main/java/co/com/ceiba/clean/viewmodel/HistorialesViewModel.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import co.com.ceiba.clean.di.componente.ComponenteDeCasoDeUso;
 import co.com.ceiba.clean.di.componente.DaggerComponenteDeCasoDeUso;
 import co.com.ceiba.domain.interactor.CasoDeUsoListarHistoriales;
 import co.com.ceiba.domain.modelo.Historial;
@@ -15,11 +14,9 @@ public class HistorialesViewModel extends ViewModel {
 
     private CasoDeUsoListarHistoriales casoDeUsoListarHistoriales;
 
-    private ComponenteDeCasoDeUso componenteDeCasoDeUso;
-
     @Inject
     public HistorialesViewModel(CasoDeUsoListarHistoriales casoDeUsoListarHistoriales) {
-        componenteDeCasoDeUso = DaggerComponenteDeCasoDeUso.create();
+        DaggerComponenteDeCasoDeUso.create();
         this.casoDeUsoListarHistoriales = casoDeUsoListarHistoriales;
     }
 
